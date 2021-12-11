93aa06488d2f588807e534285600025c7fd1bed6
John <crawfo58@purdue.edu>
2021-12-11 17:48:31 -0500

    Order By Search Implemented

--- src/Search.svelte
+++ src/Search.svelte
@@ -195,12 +195,13 @@
             // handle success
             console.log(response.data);
             console.log(Object.keys(response.data).length);
-            //output.innerText = ""
             const elem = document.getElementById("output")
+            elem.innerText = ""
             for (let i = 0; i < Object.keys(response.data).length; i++) {
                 var node = document.createElement("H2");
                 node.innerText = "Article ID: " + response.data[i].article_id +
                     " + Source ID: " + response.data[i].source_id +
+                    " + Topic ID: " + response.data[i].topic_id +
                     "\n Pub Date: " + response.data[i].pub_date +
                     "\n Title: " + response.data[i].title +
                     " + URL: " + response.data[i].url + "\n\n";
@@ -229,6 +230,60 @@
 
         //topic.value = '';
     }
+
+    async function searchFunc(e) {
+        e.preventDefault();
+        const form = document.getElementById('form3');
+
+        const sDropdown = form.elements['sA'];
+        console.log(sDropdown.value);
+
+        const sDropdown2 = form.elements['sO'];
+        console.log(sDropdown2.value);
+
+        let nameVal
+        if(sDropdown.value == "Source ID") nameVal = "source_id"
+        else if(sDropdown.value == "Topic ID") nameVal = "topic_id"
+        console.log(nameVal)
+
+        let sortOrder
+        if(sDropdown2.value == "Desc") sortOrder = "desc"
+        else sortOrder = "asc"
+        console.log(sortOrder)
+
+        axios.post('http://localhost:8080/search', {
+            name: nameVal,
+            order: sortOrder
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            console.log(response.data);
+            console.log(Object.keys(response.data).length);
+            const elem = document.getElementById("output")
+            elem.innerText = ""
+            for (let i = 0; i < Object.keys(response.data).length; i++) {
+                var node = document.createElement("H2");
+                node.innerText = "Article ID: " + response.data[i].article_id +
+                    " + Source ID: " + response.data[i].source_id +
+                    " + Topic ID: " + response.data[i].topic_id +
+                    "\n Pub Date: " + response.data[i].pub_date +
+                    "\n Title: " + response.data[i].title +
+                    " + URL: " + response.data[i].url + "\n\n";
+                elem.appendChild(node);
+            }
+        })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+        //topic.value = '';
+    }
 </script>
 
 <main>
@@ -248,9 +303,23 @@
         <div class="row"><input class="place-holder-center" type="text" id="title" placeholder="Enter Title"></div>
         <div class="row"><input class="place-holder-center" type="text" id="url" placeholder="Enter URL"></div>
         <div class="row"><input type="submit" value="Enter Article" id="t" class="full" on:click={insertFunc}></div>
-        <div class="row"><input type="submit" value="Display Articles" id="s" class="full" on:click={submitFunc}></div>
+        <div class="row"><input type="submit" value="Display All Articles" id="s" class="full" on:click={submitFunc}></div>
         <div class="row"><input class="place-holder-center" type="text" id="aID2" placeholder="Enter Article ID to Delete"></div>
         <div class="row"><input type="submit" value="Delete Article" id="d" class="full" on:click={deleteFunc}></div>
+    </form>
+
+    <form id="form3" path="post">
+        <div class="row">
+            <select class="place-holder-center" name="aSearch" id="sA">
+                <option value="Source ID">Source ID</option>
+                <option value="Topic ID">Topic ID</option>
+            </select>
+            <select class="place-holder-center" name="aOrder" id="sO">
+                <option value="Asc">Asc</option>
+                <option value="Desc">Desc</option>
+            </select>
+        </div>
+        <div class="row"><input type="submit" value="Display Sorted Articles" id="dA" class="full" on:click={searchFunc}></div>
     </form>
 
     <div id="output"></div>
